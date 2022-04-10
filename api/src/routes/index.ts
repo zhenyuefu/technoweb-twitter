b5fd9f65798889a972113ee58cb0324267324abf
zhenyue <zhenyuef@gmail.com>
2022-04-10 17:51:23 +0200

    vercel

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,6 +3,10 @@ import userRouter from "./user";
 import authRouter from "./auth";
 
 const routes: (app: express.Application) => void = (app) => {
+  app.all("/api", (req, res) => {
+    // res.setHeader("Content-Type", "text/html");
+    res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  });
   app.use("/api/user", userRouter);
   app.use("/api/auth", authRouter);
 };
