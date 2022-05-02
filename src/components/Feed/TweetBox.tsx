b90e7fe168c3b6c49db626c378d3a4b26c58acd9
zhenyue <zhenyuef@gmail.com>
2022-05-02 20:45:20 +0200

    tweetBox 图片api

--- src/components/Feed/TweetBox.tsx
+++ src/components/Feed/TweetBox.tsx
@@ -1,80 +1,116 @@
-import {PhotoCamera} from "@mui/icons-material";
-import {Avatar, Button, IconButton, TextField} from "@mui/material";
+import {Avatar, TextField} from "@mui/material";
 import React, {useState} from "react";
 import "../../style/TweetBox.css";
-import ImgBox from "./ImgBox";
-import AlertBar from "../AlertBar";
+import {Button, Image, Message, Upload} from "@arco-design/web-react";
+import {UploadItem} from "@arco-design/web-react/es/Upload";
 
 function TweetBox() {
-    const [imgList, setImgList] = useState<Array<string>>([]);
-    const [open, setOpen] = useState<boolean>(false);
 
-    const handleAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (!e.target.files) {
-            return;
+  const [disabled, setDisabled] = React.useState(false)
+  const [imgInfos, setImgInfos] = React.useState<any[]>([])
+
+
+  const onSubmit = (e: Event) => {
+    e.stopPropagation();
+  };
+
+  const onChange = (files: UploadItem[], file: UploadItem) => {
+    const imgList: any[] = [];
+    files.forEach(file => {
+      if (file.status === "done") {
+        imgList.push((file.response as any).data);
+      }
+    });
+    if (file.status === "error") {
+      Message.error(((file.response) as any).data.error.message);
+    }
+    setImgInfos(imgList)
+    setDisabled(files.some((x) => x.status !== 'done'))
+  };
+
+
+  const onRemove = (file: UploadItem) => {
+    if (file.status === "done") {
+      setDisabled(true)
+      const deleteHash = (file.response as any).data.deletehash;
+      return fetch('https://api.imgur.com/3/image/' + deleteHash, {
+          method: 'DELETE',
+          headers: {
+            Authorization: 'Client-ID ' + import.meta.env.VITE_IMGUR_CLIENT_ID,
+          }
         }
-        if (e.target.files.length + imgList.length > 4) {
-            setOpen(true);
-            return;
-        }
-        const img = e.target.files[0];
-        const reader = new FileReader();
-        reader.onload = () => {
-            setImgList([...imgList, reader.result as string]);
-            e.target.value = "";
-        };
-        reader.readAsDataURL(img);
-    };
+      ).then(res => {
+        setDisabled(false)
+        res.json()
+      })
+    }
+  }
 
-    const handleDeleteImage = (index: number) => {
-        setImgList(imgList.filter((_, i) => i !== index));
-    };
+  const [imgPreview, setImgPreview] = useState({
+    visible: false,
+    src: "",
+  });
 
-    return (
-        <div className="tweetbox">
-            <AlertBar open={open} message={"You can upload up to 4 images"} severity={"info"}
-                      onClose={() => setOpen(false)}
-                      vertical={"bottom"} horizontal={"center"}/>
-            <div style={{paddingTop: "4px", marginRight: "12px"}}>
-                <Avatar/>
-            </div>
-            <div className="tweetbox__input">
-                <TextField
-                    placeholder="What's happening?"
-                    type="text"
-                    variant="standard"
-                    maxRows={5}
-                    multiline
-                    fullWidth
-                    InputProps={{disableUnderline: true}}
-                />
+  const visibleChange = (v: boolean) => {
+    setImgPreview({
+      src: "",
+      visible: v,
+    });
+  };
 
-                {imgList.length > 0 && (
-                    <ImgBox img_list={imgList} handleDelete={handleDeleteImage}/>
-                )}
-                <div className="tweetbox__buttons">
-                    <label htmlFor="icon-button-file">
-                        <input
-                            hidden
-                            accept="image/jpeg, image/png, image/gif, image/webp"
-                            id="icon-button-file"
-                            type="file"
-                            multiple={true}
-                            onChange={handleAddImage}
-                        />
-                        <IconButton
-                            color="primary"
-                            aria-label="upload picture"
-                            component="span"
-                        >
-                            <PhotoCamera/>
-                        </IconButton>
-                    </label>
-                    <Button className="tweetbox__tweetbutton">Tweet</Button>
-                </div>
-            </div>
+  return (
+    <div className="tweetbox">
+      <Image.Preview
+        src={imgPreview.src}
+        visible={imgPreview.visible}
+        onVisibleChange={visibleChange}
+      />
+
+      <div style={{paddingTop: "4px", marginRight: "12px"}}>
+        <Avatar/>
+      </div>
+      <div className="tweetbox__input">
+        <TextField
+          placeholder="What's happening?"
+          type="text"
+          variant="standard"
+          maxRows={5}
+          multiline
+          fullWidth
+          InputProps={{disableUnderline: true}}
+        />
+
+        <div className="tweetbox__picture">
+          <Upload
+            multiple
+            listType="picture-card"
+            limit={4}
+            accept="image/png, image/jpeg, image/gif, image/tiff, image/webp"
+            onPreview={(file) => {
+              setImgPreview({
+                src: URL.createObjectURL(file.originFile as Blob),
+                visible: true,
+              });
+            }}
+            onChange={onChange}
+            action="https://api.imgur.com/3/image"
+            headers={{Authorization: "Client-ID " + import.meta.env.VITE_IMGUR_CLIENT_ID}}
+            name="image"
+            onExceedLimit={() => Message.info("You can upload up to 4 images")}
+            onRemove={onRemove}
+          />
         </div>
-    );
+        <div className="tweetbox__buttons">
+          <Button
+            type="primary"
+            disabled={disabled}
+            className="tweetbox__tweetbutton"
+            onClick={onSubmit}>Tweet</Button>
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TweetBox;
--- src/main.tsx
+++ src/main.tsx
@@ -13,14 +13,21 @@ import '@icon-park/react/styles/index.css';
 const container = document.getElementById("root") as Element;
 const root = createRoot(container);
 
+const colorName = 'blue';
+
+Array.from({length: 10}).forEach((_, index) => {
+  document.body.style.setProperty(`--primary-${index + 1}`, `var(--${colorName}-${index + 1})`)
+})
+
+
 root.render(
   // <React.StrictMode>
-    <ViewportProvider>
-      <RecoilRoot>
-        <BrowserRouter>
-          <App/>
-        </BrowserRouter>
-      </RecoilRoot>
+  <ViewportProvider>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </RecoilRoot>
     </ViewportProvider>
   // </React.StrictMode>
 );
