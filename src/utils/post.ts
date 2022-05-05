6f9ee7cafb769aba0ea9517c48f9bd81f9212e59
zhenyue <zhenyuef@gmail.com>
2022-05-05 14:17:05 +0200

    post api地址

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -1,17 +1,20 @@
-import {IFormPost} from "../types";
-import {handleResponse} from "./utils";
+import { IFormPost } from "../types";
+import { handleResponse } from "./utils";
 
 export async function addPost(data: IFormPost) {
   try {
-    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/post`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-      credentials: "include",
-      mode: "cors",
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/post`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+        credentials: "include",
+        mode: "cors",
+      }
+    );
     return handleResponse(response);
   } catch (error) {
     console.log(error);
@@ -20,18 +23,20 @@ export async function addPost(data: IFormPost) {
 
 export async function addComment(postId: string, content: string) {
   try {
-    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/comment`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        postId,
-        content,
-      }),
-      mode: "cors",
-      credentials: "include"
-    })
+    const response = await fetch(
+      `${import.meta.env.VITE_API_BASE_URL}/api/post/${postId}/comment`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          content,
+        }),
+        mode: "cors",
+        credentials: "include",
+      }
+    );
     return handleResponse(response);
   } catch (error) {
     console.log(error);
